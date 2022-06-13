7e690e0a19e93fd7bb16d9703067ba9923f324a6
Robby Hong <sky0975657713@gmail.com>
2022-06-13 17:13:38 +0800

    PAGE BG

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import styles from "../styles/index.module.css";
 import Navigation from "../componet/navigation";
 import Home from "./home";
 import About from "./about";
+import Contact from "./contact";
 
 const index: NextPage = () => {
   const [currentTab, setCurrentTab] = useState("ABOUT");
@@ -14,17 +15,19 @@ const index: NextPage = () => {
         return <Home />;
       case "ABOUT":
         return <About />;
+      case "CONTACT":
+        return <Contact />;
       default:
         return <Home />;
     }
   };
   return (
-    <>
+    <div className={styles.container}>
       {currentPage()}
       <div className={styles.Navigation}>
         <Navigation currentTab={currentTab} setCurrentTab={setCurrentTab} />
       </div>
-    </>
+    </div>
   );
 };
 
